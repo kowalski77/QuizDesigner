8ffdea2fd76a28afdb4e05c1d4ccff631526a5ea
Christian Aranda <christian.aranda@contractors.roche.com>
2021-08-08 17:42:57 +0200

    queries, 2

--- Source/QuizDesigner.Persistence/QuestionsProvider.cs
+++ Source/QuizDesigner.Persistence/QuestionsProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Arch.Utils.Functional.Monads;
@@ -17,15 +18,57 @@ namespace QuizDesigner.Persistence
             this.contextFactory = contextFactory ?? throw new ArgumentNullException(nameof(contextFactory));
         }
 
-
         public async Task<PaginatedModel<QuestionDto>> GetQuestionsAsync(QuestionsQuery questionsQuery)
         {
-            throw new NotImplementedException();
+            if (questionsQuery == null) throw new ArgumentNullException(nameof(questionsQuery));
+
+            await using var context = this.contextFactory.CreateDbContext();
+            SetContextQueryOptions(context);
+
+            var questions = context.Questions!
+                .Map()
+                .SortQuestionsBy(questionsQuery.SortByOptions, questionsQuery.AscendingSort)
+                .FilterQuestionsBy(questionsQuery.FilterByOptions, questionsQuery.FilterValue);
+
+            var paginatedModel = new PaginatedModel<QuestionDto>(questions, questionsQuery.PageNumber, questionsQuery.PageSize);
+            paginatedModel.Page();
+
+            return paginatedModel;
         }
 
         public async Task<Maybe<QuestionDto>> GetQuestionAsync(Guid id, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            await using var context = this.contextFactory.CreateDbContext();
+            SetContextQueryOptions(context);
+
+            var question = await context.Questions!.Include(x => x.Answers)
+                .FirstOrDefaultAsync(x => x.Id == id, cancellationToken)
+                .ConfigureAwait(false);
+
+            if (question == null)
+            {
+                return new Maybe<QuestionDto>();
+            }
+
+            var questionDto = new QuestionDto
+            {
+                Id = question.Id,
+                Text = question.Text,
+                Tag = question.Tag,
+                AnswerCollection = question.Answers.Select(x=> new AnswerDto
+                {
+                    Text = x.Text,
+                    IsCorrect = x.IsCorrect
+                })
+            };
+
+            return questionDto;
+        }
+
+        private static void SetContextQueryOptions(DbContext context)
+        {
+            context.ChangeTracker.AutoDetectChangesEnabled = false;
+            context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
         }
     }
 }
--- Source/QuizDesigner.Services/Queries/Dtos.cs
+++ Source/QuizDesigner.Services/Queries/Dtos.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace QuizDesigner.Services
+namespace QuizDesigner.Services.Queries
 {
     public class QuestionDto
     {
