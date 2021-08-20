4bb1ebf90ee83a56109ccd34a62a128373f75476
Christian Aranda <christian.aranda@contractors.roche.com>
2021-08-20 16:52:56 +0200

    add Quiz entity

---
+++ Source/QuizDesigner.Services/Domain/Quiz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Arch.Utils.DomainDriven;
+
+namespace QuizDesigner.Services
+{
+    public class Quiz : Entity, IAggregateRoot
+    {
+        public Quiz(string name, string examName, 
+            DateTime createdOn, ICollection<Question> questions)
+        {
+            this.Name = name;
+            this.ExamName = examName;
+            this.CreatedOn = createdOn;
+            this.Questions = questions;
+            this.Draft = false;
+        }
+
+        public string Name { get; private set; }
+
+        public void SetName(string newName)
+        {
+            this.Name = newName;
+        }
+
+        public string ExamName { get; set; }
+
+        public DateTime CreatedOn { get; private set; }
+
+        public bool Draft { get;  set; }
+
+        public ICollection<Question> Questions { get; set; }
+    }
+}
