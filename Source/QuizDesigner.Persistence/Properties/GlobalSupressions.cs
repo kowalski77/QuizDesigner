5b06095e02e453275b94f174a1e62a77b4943ddb
Christian Aranda <christian.aranda@contractors.roche.com>
2021-08-28 15:23:13 +0200

    remove warnings

--- Source/QuizDesigner.Persistence/Properties/GlobalSupressions.cs
+++ Source/QuizDesigner.Persistence/Properties/GlobalSupressions.cs
@@ -1,3 +1,8 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 [assembly:CLSCompliant(false)]
+
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "EF Auto generated code", Scope = "member", Target = "~M:QuizDesigner.Persistence.Migrations.Initial.Up(Microsoft.EntityFrameworkCore.Migrations.MigrationBuilder)")]
+
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "EF Auto generated code", Scope = "member", Target = "~M:QuizDesigner.Persistence.Migrations.Initial.Down(Microsoft.EntityFrameworkCore.Migrations.MigrationBuilder)")]
