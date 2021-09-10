e936c02f83d713c7b09d36f4c0fd1ae926556805
Christian Aranda Diaz <c.aranda.diaz@avanade.com>
2021-09-10 08:52:07 +0200

    Queue clients dictionary

--- Source/QuizDesigner.AzureQueueStorage/AzureStorageQueuePublisher.cs
+++ Source/QuizDesigner.AzureQueueStorage/AzureStorageQueuePublisher.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,6 +13,8 @@ namespace QuizDesigner.AzureQueueStorage
 {
     public class AzureStorageQueuePublisher : IMessagePublisher
     {
+        private static readonly ConcurrentDictionary<Type, QueueClient> QueueClients = new();
+
         private readonly AzureQueueStorageOptions options;
 
         public AzureStorageQueuePublisher(IOptions<AzureQueueStorageOptions> options)
@@ -18,7 +22,7 @@ namespace QuizDesigner.AzureQueueStorage
             this.options = options?.Value ?? throw new ArgumentNullException(nameof(options));
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "Azure Queue naming conventions")]
+        [SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "Azure Queue naming conventions")]
         public async Task PublishAsync(IIntegrationEvent integrationEvent, CancellationToken cancellationToken = default)
         {
             if (integrationEvent == null)
@@ -26,8 +30,8 @@ namespace QuizDesigner.AzureQueueStorage
                 throw new ArgumentNullException(nameof(integrationEvent));
             }
 
-            // TODO: reuse client
-            var queueClient = new QueueClient(this.options.StorageConnectionString, integrationEvent.GetType().Name.ToLowerInvariant());
+            var queueClient = QueueClients.GetOrAdd(integrationEvent.GetType(), type => new QueueClient(this.options.StorageConnectionString, type.Name.ToLowerInvariant()));
+
             await queueClient.CreateIfNotExistsAsync(cancellationToken: CancellationToken.None).ConfigureAwait(true);
 
             var serializedIntegrationEvent = JsonSerializer.Serialize(integrationEvent, integrationEvent.GetType());
