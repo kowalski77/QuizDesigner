a5c1ed38b78c8824fbd78b1b6742673792676fa3
Christian Aranda Diaz <c.aranda.diaz@avanade.com>
2021-09-16 20:03:34 +0200

    Create DTOs classes containers

--- Source/QuizDesigner.Application/Dtos/QuizDtos.cs
+++ Source/QuizDesigner.Application/Dtos/QuizDtos.cs
@@ -4,4 +4,6 @@ using System.Collections.Generic;
 namespace QuizDesigner.Application
 {
     public sealed record CreateQuizDto(string Name, string ExamName, IEnumerable<Guid> QuestionIdCollection);
+
+    public sealed record UpdateQuizDto(Guid QuizId, string Name, string ExamName, IEnumerable<Guid> QuestionIdCollection);
 }
--- Source/QuizDesigner.Application/Dtos/UpdateQuizDto.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace QuizDesigner.Application
-{
-    public record UpdateQuizDto(Guid QuizId, string Name, string ExamName, IEnumerable<Guid> QuestionIdCollection);
-}
