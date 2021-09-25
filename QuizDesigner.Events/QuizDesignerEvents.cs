9dedd8100fd22df729f22cf2eeb04db1f2f10ca4
Christian Aranda Diaz <c.aranda.diaz@avanade.com>
2021-09-25 18:04:57 +0200

    Fix create quiz event naming

--- QuizDesigner.Events/QuizDesignerEvents.cs
+++ QuizDesigner.Events/QuizDesignerEvents.cs
@@ -4,7 +4,7 @@ using QuizCreatedEvents;
 
 namespace QuizDesigner.Events
 {
-    public sealed record QuizCreated(Guid Id, string Category, string Exam, IEnumerable<ExamQuestion> ExamQuestionCollection) : IIntegrationEvent;
+    public sealed record QuizCreated(Guid Id, string Exam, string Category, IEnumerable<ExamQuestion> ExamQuestionCollection) : IIntegrationEvent;
 
     public sealed record ExamQuestion(string Text, string Tag, int Difficulty, IEnumerable<ExamAnswer> ExamAnswerCollection);
 
